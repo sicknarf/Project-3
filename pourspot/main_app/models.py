b4419d89ec347a0bfbf86105bc32082187ccba5f
sicknarf <fgacad@gmail.com>
2022-09-18 23:26:15 -0700

    added basic infrastructure and functions

---
+++ pourspot/main_app/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+SKILL = (
+    ('B', 'Beginner'),
+    ('I', 'Intermediate'),
+    ('A', 'Advanced')
+)
+
+# I might have over-complicated this. let me know if this is acceptable, or otherwise not
+INGREDIENT_TYPE = (
+    ('S', 'Spirit'),
+    ('R', 'Sour'),
+    ('F', 'Fizz'),
+    ('M', 'Smash'),
+    ('B', 'Bitters'),
+    ('H', 'Herb'),
+    ('J', 'Juice'),
+    ('G', 'Garnish'),
+    ('O', 'Other')
+)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.CharField(
+        max_length = 1,
+        choices = INGREDIENT_TYPE,
+        default = INGREDIENT_TYPE[0][0]
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('ingredients_detail', kwargs={'pk': self.id})
+        pass
+
+class Drink(models.Model):
+    name = models.CharField(max_length=100)  # HAS A
+    description = models.TextField(max_length=250)
+    
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    ingredients: models.ManyToManyField(Ingredient)
+    skill_level = models.CharField(
+        max_length = 1,
+        choices = SKILL,
+        default = SKILL[1][0]
+    )
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # return f"a {len(self.ingredients)} ingredient recipe for {self.drink}"
+        pass
+
---
+++ pourspot/main_app/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # http://localhost:8000
+    path('', views.home, name='home'),
+    # http://localhost:8000/about-us/
+    path('about-us/', views.about_us, name='about_us'),
+    # http://localhost:8000/drinks/
+    path('drinks/', views.drink_index, name='index'),
+    # http://localhost:8000/drinks/1/
+    path('drinks/<int:drink_id>/', views.drinks_detail, name='detail'),
+    # new route used to show a form and create a drink.
+    # http://localhost:8000/drinks/create/
+    path('drinks/create/', views.DrinkCreate.as_view(), name='drinks_create'),
+    # http://localhost:8000/drinks/2/add_feeding/
+    path('drinks/<int:drink_id>/add_recipe/', views.add_recipe, name="add_recipe"),
+
+    # these links needs to be formatted so it takes us to recipes & ingredients
+    # path('drinks/<int:drink>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+    # path('drinks/<int:drink>/add_photo', views.add_photo, name='add_photo'),
+
+    # http://localhost:8000/ingredients/
+    path('ingredients/', views.IngredientList.as_view(), name='ingredients_index'),
+
+    # http://localhost:8000/drinks/1/recipes/1/
+    ###################### NEEDS ATTENTION ######################
+    # this was previously <int:pk> instead of <int:drink_id> and <int:recipe_id>
+    path('ingredients/<int:drink_id>/recipes/<int:recipe_id>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+
+    ###################### NEEDS ATTENTION ######################
+    # http://localhost:8000/drinks/1/recipes/create/
+    path('ingredients/<int:drink_id>/recipes/create/', views.RecipeCreate.as_view(), name='recipe_create'),
+
+    ###################### NEEDS ATTENTION ######################
+    # http://localhost:8000/drinks/1/recipes/1/delete/
+    path('ingredients/<int:drink_id>/recipes/<int:recipe_id>/delete', views.RecipeDelete.as_view(), name='recipe_delete'),
+    
+    path('accounts/signup', views.signup, name='signup'),
+]
