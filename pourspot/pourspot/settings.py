dbacba435093e61fd2d813997487d4214b82ef0e
sicknarf <fgacad@gmail.com>
2022-09-22 09:15:48 -0700

    updated recipe schema and added colors

--- pourspot/pourspot/settings.py
+++ pourspot/pourspot/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
 # Add this variable to specify where successful logins should redirect to
 LOGIN_REDIRECT_URL = '/drinks/'
 # Add this variable to specify where logging out redirects to
